445e57869659452333a677abc1fee0c93bcbe10a
jamiscript <jah.passaro@gmail.com>
2020-04-19 15:41:58 -0300

    login screen, button component and text input components

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Login from './screens/Login';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Login></Login>
     </View>
   );
 }
