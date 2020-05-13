478477628a40dd5a56dc99a407b957cb2d4144b0
Letícia Vigna <leticia.vigna@hotmail.com>
2020-05-13 13:12:03 -0300

    CustomButton com duas opções de estilo

--- app/components/CustomButton.js
+++ app/components/CustomButton.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
-
-const CustomButton = (myProp) => {
-    return (
-        <View>
-            <TouchableOpacity onPress={myProp.action}>
-                <Text style={styles.button}>
-                    {myProp.btnName}
-                </Text>
-            </TouchableOpacity>
-        </View>
-    );
+const CustomButton = props => {
+  return (
+    <View>
+      <TouchableOpacity onPress={props.action}>
+        <Text style={props.main ? styles.button : styles.button2}>
+          {props.name}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        //width: 105,
-        alignSelf: 'center',
-        textAlign: 'center',
-        marginTop: 10,
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        backgroundColor: '#757575',
-        color: 'white',
-        //no android funciona
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#757575',
-    }
+  button: {
+    textAlign: 'center',
+    alignSelf: 'center',
+    marginTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    color: 'black',
+    backgroundColor: '#feee35', // Amarelo
+  },
+  button2: {
+    textAlign: 'center',
+    alignSelf: 'center',
+    marginTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    color: '#9967bf' // Roxo médio
+  }
 });
 
 export default CustomButton;
